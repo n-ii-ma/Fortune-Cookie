c11d4ed5b81d0e54a25b6b5ab390523f59de4f32
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-08-10 16:20:33 +0430

    Final edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Random messages to be displayed
-const greetings = ['Hello', 'Hi', 'Howdy', 'Lovely day', 'Hey'];
+const greetings = ['Hello', 'Hi', 'Howdy', 'Greetings', 'Hey'];
 const messages = ['Be not afraid of growing slowly, be afraid only of standing still.', 'Your road to glory will be rocky, but fulfilling.', 
 'Set yourself up to experience what you love.', 'All you need is love.', 'Don’t pursue happiness – create it.', 'Success lies in the hands of those who wants it.', 
 'People learn little from success, but much from failure.', 'A ship in harbor is safe, but that’s not why ships are built.', 'All things are difficult before they are easy.', 
